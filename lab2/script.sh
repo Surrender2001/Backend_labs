273d76b1338497a2184ed7a0af82cddcd42e511b
Sergey <79881314+Surrender2001@users.noreply.github.com>
2022-03-23 17:15:25 +1000

    Add files via upload

---
+++ lab2/script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Hello! My nickname is $1"
+echo "$2 is the best anime title ever!"
+echo "$3 is my favourite streamer"
+echo "Hearthstone is random $4, don't play it!"
+echo "-----------SUCCESS------------"
+find . -name *.txt*
+file out.txt
